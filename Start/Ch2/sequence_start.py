bcfc5b7ebdb911b6ab5636f7bead1c281031e1d1
Satakshi Shanvi <81086303+shanvii@users.noreply.github.com>
2025-11-10 11:05:16 +0000

    commit

--- Start/Ch2/sequence_start.py
+++ Start/Ch2/sequence_start.py
@@ -3,26 +3,48 @@
 
 # Sequences: Lists and Tuples
 # These are -- surprise -- sequences of values
-
+mylist = [1, 2, "shanvi", False]
+print(mylist)
+print(len(mylist))
 
 # to access a member of a sequence type, use []
-
+print(mylist[2])
+print(mylist[-1])
 
 # add a list to another list
+anotherlist = [3, 4]
+print(anotherlist)
 
+# mylist = anotherlist + mylist
+mylist = mylist + anotherlist
+print(mylist)
+
+mystr = "this is python" #immutable
+print(len(mystr))
+print(mystr[2])
 
 # use slices to get parts of a sequence
-
+print(mylist[1:4])
+print(mylist[1:5:2])
+print(mylist[::2])
 
 # you can use slices to reverse a sequence
-
+print(mylist[::-1])
+print(mylist[:2:-1])
+print(mystr[::-1])
 
 # Tuples are like lists, but they are immutable
-
+mytuple = (1, 2, "name", 0)
+print(mytuple[1])
 
 # Sets are also sequences, but they contain unique values
+myset = {1, 2, 2, 3, 4, 2}
+print(myset)
 
 # Set, however, can not be indexed like lists or tuples
 # print(myset[0]) # this will cause an error
 
 # Test for membership
+print(1 in mylist)
+print(2 in mytuple)
+print(3 in myset)
--- Start/Ch2/variables_start.py
+++ Start/Ch2/variables_start.py
@@ -10,12 +10,24 @@ mystr = "This is a string"
 mybool = True
 
 # We can display the content of a variable using the print() function
-
+print(myint)
+print(mystr)
 
 # Operators are used to perform operations on variables
+print(myint + myfloat)
+print(myint * myint)
+print(myint * myfloat)
+print(myint / myfloat)
+print(myint % 3)
 
+anotherstr = "This is another string"
+print(mystr + " " + anotherstr)
+print("hello " * 3)
 
 # Logical and comparison operators 
-
+# == != < >
+# and or not
 
 # re-declaring a variable works
+myint = "abc"
+print(myint)
